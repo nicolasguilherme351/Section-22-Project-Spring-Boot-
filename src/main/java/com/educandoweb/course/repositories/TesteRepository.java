cd2b07ca4eb1b917cc8fdde9df2558b3951e069d
nicolasrenovado12 <nicolasrenovado12@gmail.com>
2025-02-09 13:06:37 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/educandoweb/course/repositories/TesteRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.Teste;
+
+public interface TesteRepository extends JpaRepository<Teste, Long>{
+
+}
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +10,12 @@ import com.educandoweb.course.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
+@CrossOrigin(origins = "http://localhost:9000") // Allow frontend URL for CORS
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok(u);
-		
-	}
-
-	
-	
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok(u);
+    }
 }
