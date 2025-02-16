299378d0cef5ffbc46577e1b78c9eab327ba6fad
nicolasrenovado12 <nicolasrenovado12@gmail.com>
2025-02-16 12:56:59 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +18,7 @@ import jakarta.persistence.Table;
 @Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,10 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	@OneToMany(mappedBy = "client")
 	private List<Order> listOrder = new ArrayList<>();
-	
+
 	public User() {
 	}
 
@@ -100,7 +102,5 @@ public class User implements Serializable {
 	public List<Order> getListOrder() {
 		return listOrder;
 	}
-	
-	
-	
+
 }
