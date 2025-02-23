8f5404e9fd161695eb76ab960d069a3f1699f904
nicolasrenovado12 <nicolasrenovado12@gmail.com>
2025-02-22 21:11:31 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -9,9 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Book;
+import com.educandoweb.course.entities.Department;
+import com.educandoweb.course.entities.Employee;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.BookRepository;
+import com.educandoweb.course.repositories.DepartmentRepository;
+import com.educandoweb.course.repositories.EmployeeRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -28,8 +33,19 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Department dp1 = new Department(null, "marketing");
+		
+		Employee emp1 = new Employee(null, "João", 3100.0, dp1);
 		
 		
 		Book book1 = new Book(null, 49.90, "Rogerio de santos farjado", "Lógica matemática");
@@ -38,13 +54,16 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		bookRepository.saveAll(Arrays.asList(book1, book2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		departmentRepository.saveAll(Arrays.asList(dp1));
+		employeeRepository.saveAll(Arrays.asList(emp1));
 	}
 	
 }
